4a64e8c44a0899275151b32dc5351201e3f7616c
shvetsov-as <a002eshvetsov@gmail.com>
2022-06-22 17:53:31 +0300

    "find user-full", "find all users-short" actions complete

--- src/main/java/com/example/testtask/entity/user/User.java
+++ src/main/java/com/example/testtask/entity/user/User.java
@@ -47,7 +47,7 @@ public class User {
         this.roles = roles;
     }
 
-    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(name = "users_roles",
             joinColumns = @JoinColumn(name = "user_login"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
--- src/main/java/com/example/testtask/model/service/dataservice/user/UserDataServiceImpl.java
+++ src/main/java/com/example/testtask/model/service/dataservice/user/UserDataServiceImpl.java
@@ -1,8 +1,11 @@
 package com.example.testtask.model.service.dataservice.user;
 
+import com.example.testtask.entity.user.User;
 import com.example.testtask.entity.user.mapper.UserMapper;
 import com.example.testtask.model.service.requestservice.RequestCheckService;
 import com.example.testtask.repo.UserRepository;
+import com.example.testtask.webservice.jaxb.GetAllUsersResponse;
+import com.example.testtask.webservice.jaxb.GetUserResponse;
 import com.example.testtask.webservice.jaxb.StatusResponse;
 import com.example.testtask.webservice.jaxb.UserFull;
 import com.example.testtask.webservice.jaxb.UserShort;
@@ -11,7 +14,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class UserDataServiceImpl implements UserDataService{
+public class UserDataServiceImpl implements UserDataService {
 
     private final UserRepository userRepository;
     private final RequestCheckService requestCheckService;
@@ -24,13 +27,28 @@ public class UserDataServiceImpl implements UserDataService{
     }
 
     @Override
-    public UserFull findUserShort(String login) {
-        return null;
+    public GetUserResponse findUserFull(String login) {
+
+        GetUserResponse userResponse = new GetUserResponse();
+        userResponse.setUserFull(
+                userMapper.userToUserFull(
+                        userRepository.findById(login)
+                                .orElse(new User("User with login [ " + login + " ] not found", " ", " "))
+                )
+        );
+
+        return userResponse;
     }
 
     @Override
-    public List<UserShort> findAllUsersFull() {
-        return null;
+    public GetAllUsersResponse findAllUsersShort() {
+
+        GetAllUsersResponse allUsersResponse = new GetAllUsersResponse();
+        allUsersResponse.getUserShort().addAll(
+                userMapper.userToUserShortList(userRepository.findAll())
+        );
+
+        return allUsersResponse;
     }
 
     @Override
@@ -41,12 +59,12 @@ public class UserDataServiceImpl implements UserDataService{
     @Override
     public StatusResponse create(UserFull userFull) {
 
-        StatusResponse response = new StatusResponse();
-        response.setSuccess(true);
+        StatusResponse responseCreated = new StatusResponse();
+        responseCreated.setSuccess(true);
 
         userRepository.save(userMapper.userFullToUser(userFull));
 
-        return response;
+        return responseCreated;
     }
 
     @Override
